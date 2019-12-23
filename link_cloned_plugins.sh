0f2285e9eb9e46c9422fb1ee32ea28c549d5042b
Abdullah Zafar <mabdullahzafar08@gmail.com>
2019-12-23 17:56:27 +0500

    Make plugin link/clone files a part of devstack

---
+++ link_cloned_plugins.sh
@@ -0,0 +1,35 @@
+cd nodebb-plugin-s3-uploads
+npm link
+cd .. && cd ..
+npm link nodebb-plugin-s3-uploads && ./nodebb activate nodebb-plugin-s3-uploads
+cd plugins
+
+cd nodebb-plugin-write-api
+npm link
+cd .. && cd ..
+npm link nodebb-plugin-write-api && ./nodebb activate nodebb-plugin-write-api
+cd plugins
+
+cd nodebb-plugin-custom-join
+npm link
+cd .. && cd ..
+npm link nodebb-plugin-custom-join && ./nodebb activate nodebb-plugin-custom-join
+cd plugins
+
+cd nodebb-plugin-session-sharing
+npm link
+cd .. && cd ..
+npm link nodebb-plugin-session-sharing && ./nodebb activate nodebb-plugin-session-sharing
+cd plugins
+
+cd nodebb-theme-philu-community
+npm link
+cd .. && cd ..
+npm link nodebb-theme-philu-community && ./nodebb activate nodebb-theme-philu-community
+cd plugins
+
+cd nodebb-plugin-philu-badging
+npm link
+cd .. && cd ..
+npm link nodebb-plugin-philu-badging && ./nodebb activate nodebb-plugin-philu-badging
+
---
+++ plugin_clone.sh
@@ -0,0 +1,6 @@
+git clone git@github.com:philanthropy-u/nodebb-plugin-custom-join.git
+git clone git@github.com:philanthropy-u/nodebb-plugin-s3-uploads.git
+git clone git@github.com:NodeBB/nodebb-plugin-write-api.git
+git clone git@github.com:philanthropy-u/nodebb-plugin-session-sharing.git
+git clone git@github.com:philanthropy-u/nodebb-theme-philu-community.git
+git clone git@github.com:philanthropy-u/nodebb-plugin-philu-badging.git
