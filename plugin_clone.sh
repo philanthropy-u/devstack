c157c083e66cf346516f51bc68a5b6e40eadfa23
Abdullah Zafar <mabdullahzafar08@gmail.com>
2020-01-08 13:16:52 +0500

    Add plugin related files to devstack

---
+++ plugin_clone.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/philanthropy-u/nodebb-plugin-custom-join.git
+git clone https://github.com/philanthropy-u/nodebb-plugin-s3-uploads.git
+git clone https://github.com/philanthropy-u/nodebb-plugin-write-api.git
+git clone https://github.com/philanthropy-u/nodebb-plugin-session-sharing.git
+git clone https://github.com/philanthropy-u/nodebb-theme-philu-community.git
+git clone https://github.com/philanthropy-u/nodebb-plugin-philu-badging.git
+
