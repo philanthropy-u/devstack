de2502ebca397d09e2d313dcc81c9282619cd399
aliassad <aliassad2@hotmail.com>
2020-02-12 16:52:45 +0500

    LP-1780 Discovery and Credentials Added start and stop commands for both applications also added them in philu provision_philu.sh

--- provision_philu.sh
+++ provision_philu.sh
@@ -42,6 +42,8 @@ docker exec -i edx.devstack.mongo mongo < mongo-provision.js
 
 # Nothing special needed for studio
 docker-compose $DOCKER_COMPOSE_FILES up -d studio
+./provision-discovery.sh
+./provision-credentials.sh
 ./provision-nodebb.sh
 
 
